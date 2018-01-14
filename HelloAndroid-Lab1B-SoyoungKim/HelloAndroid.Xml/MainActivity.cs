f64e842ecca05c96346dbc1891d9490f9a0139c2
bomnamu77 <bomnamu@gmail.com>
2018-01-13 21:41:44 -0800

    Lab1B

---
+++ HelloAndroid-Lab1B-SoyoungKim/HelloAndroid.Xml/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace HelloAndroid.Xml
+{
+    [Activity(Label = "HelloAndroid.Xml", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            var aButton = FindViewById<Button>(Resource.Id.aButton);
+            var aLabel = FindViewById<TextView>(Resource.Id.helloLabel);
+            aButton.Click += (sender, e) => {
+                aLabel.Text = "Hello from the button";
+            };
+        }
+    }
+}
+
