d50eaa2c66327fd3cffb80fd56c8702f6f516aba
bomnamu77 <bomnamu@gmail.com>
2018-02-04 00:02:00 -0800

    beta

--- Lab4.Little Pig/Lab4.Little Pig/MainActivity.cs
+++ Lab4.Little Pig/Lab4.Little Pig/MainActivity.cs
@@ -1,18 +1,125 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using PigGame;
 
 namespace Lab4.Little_Pig
 {
+    
+
     [Activity(Label = "Lab4.Little_Pig", MainLauncher = true)]
     public class MainActivity : Activity
     {
+
+        PigLogic game;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            var editTextPlayer1 = FindViewById<EditText>(Resource.Id.editTextPlayer1);
+            var editTextPlayer2 = FindViewById<TextView>(Resource.Id.editTextPlayer2);
+            var textViewPlayersTurn = FindViewById<TextView>(Resource.Id.textViewPlayersTurn);
+            if (savedInstanceState == null)     //if null, initialization
+            {
+                // Create the quote collection and load quotes
+                game = new PigLogic();
+                game.Player1Name = editTextPlayer1.Text;
+                game.Player2Name = editTextPlayer2.Text;
+                textViewPlayersTurn.Text = game.Player1Name + "'s Turn";
+            }
+
+
+
+            /*
+            editTextPlayer1.AfterTextChanged += delegate {
+                if (game.Turn == 1)
+                    textViewPlayersTurn.Text = editTextPlayer1 + "'s Turn";
+                
+            };
+            editTextPlayer2.AfterTextChanged += delegate {
+                if (game.Turn == 2)
+                    textViewPlayersTurn.Text = editTextPlayer2 + "'s Turn";
+
+            };*/
+            int roll = 0;
+            var buttonRollDie = FindViewById<Button>(Resource.Id.buttonRollDie);
+            var textViewPoint4ThisTurn = FindViewById<TextView>(Resource.Id.textViewPoint4ThisTurn);
+            var imageViewDie = FindViewById<ImageView>(Resource.Id.imageViewDie);
+            var textViewPlayer1Score = FindViewById<TextView>(Resource.Id.textViewPlayer1Score);
+            var textViewPlayer2Score = FindViewById<TextView>(Resource.Id.textViewPlayer2Score);
+
+            buttonRollDie.Click += delegate
+            {
+                roll = game.RollDie();
+                if (roll == 1)
+                    imageViewDie.SetImageResource(Resource.Drawable.Dice1);
+                else if (roll == 2)
+                    imageViewDie.SetImageResource(Resource.Drawable.Dice2);
+                else if (roll == 3)
+                    imageViewDie.SetImageResource(Resource.Drawable.Dice3);
+                else if (roll == 4)
+                    imageViewDie.SetImageResource(Resource.Drawable.Dice4);
+                else if (roll == 5)
+                    imageViewDie.SetImageResource(Resource.Drawable.Dice5);
+                else
+                    imageViewDie.SetImageResource(Resource.Drawable.Dice6);
+                textViewPoint4ThisTurn.Text = string.Format("{0}", game.TurnPoints);
+                if (roll == PigLogic.BAD_NUMBER)
+                {
+                    game.ChangeTurn();
+                    if (game.Turn == 2)
+                    {
+
+                        textViewPlayer1Score.Text = string.Format("{0}", game.Player1Score);
+
+                        textViewPlayersTurn.Text = game.Player2Name + "'s Turn";
+                    }
+                    else
+                    {
+
+                        textViewPlayer2Score.Text = string.Format("{0}", game.Player2Score);
+
+                        textViewPlayersTurn.Text = game.Player1Name + "'s Turn";
+                    }
+
+                }
+            };
+
+            
+            var buttonEndTurn = FindViewById<Button>(Resource.Id.buttonEndTurn);
+
+            buttonEndTurn.Click += delegate
+            {
+                game.ChangeTurn();
+
+                if (game.Turn == 2)
+                {
+                    
+                    textViewPlayer1Score.Text = string.Format("{0}", game.Player1Score);
+                    
+                    textViewPlayersTurn.Text = game.Player2Name + "'s Turn";
+                }
+                else
+                {
+                    
+                    textViewPlayer2Score.Text = string.Format("{0}", game.Player2Score);
+                    
+                    textViewPlayersTurn.Text = game.Player1Name + "'s Turn";
+                }
+
+
+            };
+
+            var buttonNewGame = FindViewById<Button>(Resource.Id.buttonNewGame);
+            buttonNewGame.Click += delegate
+            {
+                game.ResetGame();
+                textViewPlayer1Score.Text = textViewPlayer2Score.Text = "0";
+            };
         }
     }
 }
--- Lab4.Little Pig/Lab4.Little Pig/Resources/Resource.Designer.cs
+++ Lab4.Little Pig/Lab4.Little Pig/Resources/Resource.Designer.cs
@@ -41,11 +41,112 @@ namespace Lab4.Little_Pig
 			}
 		}
 		
-		public partial class Layout
+		public partial class Drawable
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Main = 2130837504;
+			public const int Dice1 = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int Dice2 = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int Dice3 = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int Dice4 = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int Dice5 = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int Dice6 = 2130837509;
+			
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Drawable()
+			{
+			}
+		}
+		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050011
+			public const int buttonEndTurn = 2131034129;
+			
+			// aapt resource value: 0x7f050012
+			public const int buttonNewGame = 2131034130;
+			
+			// aapt resource value: 0x7f050010
+			public const int buttonRollDie = 2131034128;
+			
+			// aapt resource value: 0x7f050004
+			public const int editTextPlayer1 = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int editTextPlayer2 = 2131034117;
+			
+			// aapt resource value: 0x7f05000c
+			public const int imageViewDie = 2131034124;
+			
+			// aapt resource value: 0x7f050000
+			public const int linearLayout1 = 2131034112;
+			
+			// aapt resource value: 0x7f050003
+			public const int linearLayout2 = 2131034115;
+			
+			// aapt resource value: 0x7f050006
+			public const int linearLayout3 = 2131034118;
+			
+			// aapt resource value: 0x7f05000f
+			public const int linearLayout4 = 2131034127;
+			
+			// aapt resource value: 0x7f050001
+			public const int textPlayer1 = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int textPlayer2 = 2131034114;
+			
+			// aapt resource value: 0x7f050009
+			public const int textViewPlayer1Score = 2131034121;
+			
+			// aapt resource value: 0x7f050007
+			public const int textViewPlayer1ScoreLabel = 2131034119;
+			
+			// aapt resource value: 0x7f05000a
+			public const int textViewPlayer2Score = 2131034122;
+			
+			// aapt resource value: 0x7f050008
+			public const int textViewPlayer2ScoreLabel = 2131034120;
+			
+			// aapt resource value: 0x7f05000b
+			public const int textViewPlayersTurn = 2131034123;
+			
+			// aapt resource value: 0x7f05000e
+			public const int textViewPoint4ThisTurn = 2131034126;
+			
+			// aapt resource value: 0x7f05000d
+			public const int textViewPoint4ThisTurnLabel = 2131034125;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
+		public partial class Layout
+		{
+			
+			// aapt resource value: 0x7f030000
+			public const int Main = 2130903040;
 			
 			static Layout()
 			{
@@ -60,8 +161,8 @@ namespace Lab4.Little_Pig
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int app_name = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int app_name = 2130968576;
 			
 			static String()
 			{
