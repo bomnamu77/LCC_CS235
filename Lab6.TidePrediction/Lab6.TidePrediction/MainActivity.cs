8b67de6c8a969fd84b25e006b519d467d4f5565b
bomnamu77 <bomnamu@gmail.com>
2018-02-24 23:56:04 -0800

    Add Lab6 assignemnt

---
+++ Lab6.TidePrediction/Lab6.TidePrediction/MainActivity.cs
@@ -0,0 +1,78 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.Views;
+using Android.OS;
+using System.Collections.Generic;
+using Android.Runtime;
+
+namespace Lab6.TidePrediction
+{
+    [Activity(Label = "Lab6.TidePrediction", MainLauncher = true)]
+    public class MainActivity : ListActivity
+    {
+        TideItem[] tideItem;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+
+            // Read and prase the vocabulary file and provide the adapter with the data
+            var reader = new XmlTideFileParser(Assets.Open(@"TideData.xml"));
+            var dataList = reader.TideList;
+
+            int itemCount = dataList.Count;
+
+            tideItem = new TideItem[itemCount];
+            
+            int i = 0;
+            //JavaDictionary<string, object> prediction = null;
+            while ( i < itemCount )
+            {
+                object date, day, time, height, hi_low;
+                //prediction = new JavaDictionary<string, object>();
+                //dataList.IndexOf(prediction, i);
+                IDictionary<string, object> prediction = dataList[i];
+                prediction.TryGetValue(XmlTideFileParser.DATE, out date);
+                prediction.TryGetValue(XmlTideFileParser.DAY, out day);
+                prediction.TryGetValue(XmlTideFileParser.TIME, out time);
+                prediction.TryGetValue(XmlTideFileParser.HEIGHT, out height);
+                prediction.TryGetValue(XmlTideFileParser.HI_LOW, out hi_low);
+
+                string sdate = date.ToString();
+                string sday = day.ToString();
+                string stime = time.ToString();
+                string sheight = height.ToString();
+                string shi_low = hi_low.ToString();
+                tideItem[i] = new TideItem(sdate, sday, stime, sheight, shi_low);
+                i++;
+
+            }
+            
+            ListAdapter = new ArrayAdapter<TideItem>(this,
+                 Android.Resource.Layout.TwoLineListItem,
+                 tideItem);
+            /*
+            // constructor takes: reference to this Activity, List of Dictionary objects, row layout, 
+            ListAdapter = new VocabAdapter(this, dataList,
+                Android.Resource.Layout.SimpleListItem1,
+                new string[] { XmlVocabFileParser.SPANISH, XmlVocabFileParser.ENGLISH },
+                new int[] { Android.Resource.Id.Text1 }
+            );*/
+            
+            // Set our view from the "main" layout resource
+            //SetContentView(Resource.Layout.Main);
+
+
+        }
+
+        // Row click event handler in ListActivity
+        protected override void OnListItemClick(ListView l,
+             View v, int position, long id)
+        {
+            string word = tideItem[position].Height;
+            Android.Widget.Toast.MakeText(this, word,
+                Android.Widget.ToastLength.Short).Show();
+        }
+    }
+}
+
---
+++ Lab6.TidePrediction/Lab6.TidePrediction/TideItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Lab6.TidePrediction
+{
+    public class TideItem
+    {
+        
+        public string Date { get; set; }
+        public string Day { get; set; }
+        public string Time { get; set; }
+        public string Height { get; set; }
+        public string Hi_Low { get; set; }
+
+
+        
+
+        public TideItem(string date, string day, string t, string h, string hl)
+        {
+            
+            Date = date;
+            Day = day;
+            Time = t;
+            Height = h;
+            Hi_Low = hl;
+        }
+/*
+        public override string ToString()
+        {
+            return Spanish;
+        }*/
+    }
+}
