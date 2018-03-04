02b851f2b8aede6bdc993924df49ad3a0fcff915
bomnamu77 <bomnamu@gmail.com>
2018-03-04 00:50:59 -0800

    Lab7B

---
+++ Lab7.TidePrediction/Lab7.TidePrediction/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Lab7.TidePrediction
+{
+    [Activity(Label = "Lab7.TidePrediction", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
